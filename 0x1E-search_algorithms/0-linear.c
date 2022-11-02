74e1aebabb457570208d502ed0d9372da1e8783c
Ahortu901 <aricktech.edu@gmail.com>
2022-11-02 12:37:14 +0000

    0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,19 @@
+/**
+ * linear_search
+ *
+ * */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < (signed int)size; i++)
+	{
+		printf("Value checked array[%i} = [%i]\n", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
